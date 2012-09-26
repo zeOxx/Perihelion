4196b7ed47e8db8f79a2a65904146fb28a9fe6b0
Snorre Brecheisen <snott_on_a_roll@hotmail.com>
2012-09-26 15:33:20 +0200

    added GameObject.cs
    made GameObject.cs and put in some attributes

---
+++ Perihelion/Perihelion/GameObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Perihelion
+{
+    class GameObject
+    {
+        public Texture2D texture;
+        public Vector2 position = new Vector2();
+        public float velocity;
+        public int direction;
+
+        public GameObject () {
+            
+        }
+    }
+}
