5ea8660c7441aedeaa950c8747b0839c28cdc7b4
Snorre Brecheisen <snott_on_a_roll@hotmail.com>
2012-09-27 13:39:24 +0200

    \Models updating
    Updated file structure of models, Unit and Player close to done.

--- Perihelion/Perihelion/Models/Player.cs
+++ Perihelion/Perihelion/Models/Player.cs
@@ -7,36 +7,26 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Perihelion.Models
 {
-    class Player : GameObject
+    class Player : Unit
     {
         private float wellMultiplier;
         private bool wellStatus;
-        private int currentHealth;
-        private int maxHealth;
-        private float damageMultiplier;
-        private float attackMultiplier;
 
         /************************************************************************/
         /*  Constructors for Player object                                      */
         /************************************************************************/
         public Player(Texture2D texture, float x, float y, Vector2 velocity, int currentHealth, int maxHealth)
-            : base(texture, x, y, velocity)
+            : base(texture, x, y, velocity, currentHealth, maxHealth)
         {
             setWellMultiplier(1);
             setWellStatus(false);
-            setHealth(currentHealth, maxHealth);
-            setDamageMultiplier(1);
-            setAttackMultiplier(1);
         }
 
-        public Player(Texture2D texture, float x, float y, Vector2 velocity, float wellMultiplier, int currentHealth, int maxHealth, float damageMultiplier, float attackMultiplier)
-            : base(texture, x, y, velocity)
+        public Player(Texture2D texture, float x, float y, Vector2 velocity, int currentHealth, int maxHealth, float damageMultiplier, float attackMultiplier, float wellMultiplier)
+            : base(texture, x, y, velocity, currentHealth, maxHealth, damageMultiplier, attackMultiplier)
         {
             setWellMultiplier(wellMultiplier);
             setWellStatus(wellStatus);
-            setHealth(currentHealth, maxHealth);
-            setDamageMultiplier(damageMultiplier);
-            setAttackMultiplier(attackMultiplier);
         }
         /************************************************************************/
         /*  Set functions for Player attributes                                 */
@@ -51,22 +41,6 @@ namespace Perihelion.Models
             this.wellStatus = wellStatus;
         }
 
-        void setHealth(int currentHealth, int maxHealth)
-        {
-            this.currentHealth = currentHealth;
-            this.maxHealth = maxHealth;
-        }
-
-        void setDamageMultiplier(float damageMultiplier)
-        {
-            this.damageMultiplier = damageMultiplier;
-        }
-
-        void setAttackMultiplier(float attackMultiplier)
-        {
-            this.attackMultiplier = attackMultiplier;
-        }
-
         /************************************************************************/
         /*  Get functions for Player attributes                                 */
         /************************************************************************/
@@ -78,25 +52,6 @@ namespace Perihelion.Models
         bool getWellStatus()
         {
             return wellStatus;
-        }
-        int getCurrentHealth()
-        {
-            return this.currentHealth;
-        }
-
-        int getMaxHealth()
-        {
-            return this.maxHealth;
-        }
-
-        float getDamageMultiplier()
-        {
-            return this.damageMultiplier;
-        }
-
-        float getAttackMultiplier()
-        {
-            return this.attackMultiplier;
         }
     }
 }
---
+++ Perihelion/Perihelion/Models/Unit.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Perihelion.Models
+{
+    class Unit : GameObject
+    {
+        private int currentHealth;
+        private int maxHealth;
+        private float damageMultiplier;
+        private float attackMultiplier;
+        
+        /************************************************************************/
+        /*  Constructors for Player object                                      */
+        /************************************************************************/
+        public Unit(Texture2D texture, float x, float y, Vector2 velocity, int currentHealth, int maxHealth)
+            : base(texture, x, y, velocity)
+        {
+            setHealth(currentHealth, maxHealth);
+            setDamageMultiplier(1);
+            setAttackMultiplier(1);
+        }
+
+        public Unit(Texture2D texture, float x, float y, Vector2 velocity, int currentHealth, int maxHealth, float damageMultiplier, float attackMultiplier)
+            : base(texture, x, y, velocity)
+        {
+            setHealth(currentHealth, maxHealth);
+            setDamageMultiplier(damageMultiplier);
+            setAttackMultiplier(attackMultiplier);
+        }
+
+        /************************************************************************/
+        /*  Set functions for Player attributes                                 */
+        /************************************************************************/
+        void setHealth(int currentHealth, int maxHealth)
+        {
+            this.currentHealth = currentHealth;
+            this.maxHealth = maxHealth;
+        }
+
+        void setDamageMultiplier(float damageMultiplier)
+        {
+            this.damageMultiplier = damageMultiplier;
+        }
+
+        void setAttackMultiplier(float attackMultiplier)
+        {
+            this.attackMultiplier = attackMultiplier;
+        }
+
+        /************************************************************************/
+        /*  Get functions for Player attributes                                 */
+        /************************************************************************/
+        int getCurrentHealth()
+        {
+            return this.currentHealth;
+        }
+
+        int getMaxHealth()
+        {
+            return this.maxHealth;
+        }
+
+        float getDamageMultiplier()
+        {
+            return this.damageMultiplier;
+        }
+
+        float getAttackMultiplier()
+        {
+            return this.attackMultiplier;
+        }
+    }
+}
