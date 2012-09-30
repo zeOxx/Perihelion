488628b9e95885cbd4f57bee56db3b903b5b9c34
Thomas Nilsen <thnilsen@broadpark.no>
2012-09-30 20:54:07 +0200

    Camera-class update
    The player can now move all over the screen instead of just left/right

--- Perihelion/Perihelion/Camera.cs
+++ Perihelion/Perihelion/Camera.cs
@@ -21,10 +21,8 @@ namespace Perihelion
 
         public void update(GameTime gameTime, cameraTest picture)
         {
-            this.center = new Vector2(picture.spritePosition.X + (picture.spriteRectangle.Width / 2) - 400, 0);
-
-            /*this.center = new Vector2(ship.spritePosition.X + (ship.spriteRectangle.Width / 2 -  400)
-                ,ship.spritePosition.Y + (ship.spriteRectangle.Height / 2 - 250));*/
+            this.center = new Vector2(picture.spritePosition.X + (picture.spriteRectangle.Width / 2 -  400)
+            ,picture.spritePosition.Y + (picture.spriteRectangle.Height / 2 - 250));
 
             this.transform = Matrix.CreateScale(new Vector3(1, 1, 0)) *
                 Matrix.CreateTranslation(new Vector3(-center.X, -center.Y, 0));
