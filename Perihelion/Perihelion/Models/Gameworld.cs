c5908ae610952529aab0beff5d3fdf9496524e86
Simen Løkken <simen.lokken@gmail.com>
2012-10-12 15:49:00 +0200

    I have no idea

--- Perihelion/Perihelion/Models/Gameworld.cs
+++ Perihelion/Perihelion/Models/Gameworld.cs
@@ -45,6 +45,8 @@ namespace Perihelion.Models
             camera.update(playerObject.getPosition());
         }
 
+
+        //Returns camera to draw function
         public Camera getCamera()
         {
             return camera;
