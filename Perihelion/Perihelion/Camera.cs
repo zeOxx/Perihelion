f50b026371fb92e40a9a92a7e7706c8dc195e805
Inge Dalby <inge.dalby@gmail.com>
2012-10-17 15:28:05 +0200

    Fixed camera class

--- Perihelion/Perihelion/Camera.cs
+++ Perihelion/Perihelion/Camera.cs
@@ -13,7 +13,7 @@ namespace Perihelion
     class Camera
     {
         private Matrix transform;
-        //private Vector2 cameraOffset;
+        private Vector2 cameraOffset;
         private Vector2 center;
         private Viewport view;
         float zoom = 1.0f;
@@ -30,7 +30,6 @@ namespace Perihelion
             get { return this.transform; }
         }
 
-        
 
         public float Zoom
         {
@@ -38,25 +37,36 @@ namespace Perihelion
             set { this.zoom = value; }
         }
 
+        public Vector2 Center
+        {
+            get { return this.center; }
+        }
 
-        
+        public Viewport View
+        {
+            get { return this.view; }
+        }
 
         public void update(Vector2 objectToFollow)
         {
-            this.center = objectToFollow; //- cameraOffset;
-
-            
-            // this.center = new Vector2(picture.spritePosition.X + (picture.spriteRectangle.Width / 2 - 400)
-            // , picture.spritePosition.Y + (picture.spriteRectangle.Height / 2 - 250));
+            this.center = objectToFollow - cameraOffset;
+            //             this.center = new Vector2(picture.spritePosition.X + (picture.spriteRectangle.Width / 2 - 400)
+            //             , picture.spritePosition.Y + (picture.spriteRectangle.Height / 2 - 250));
 
             //this.transform = Matrix.CreateScale(new Vector3(this.zoom, this.zoom, 0)) *
             //Matrix.CreateTranslation(new Vector3(-this.center.X, -this.center.Y, 0));
 
             this.transform = Matrix.CreateTranslation(new Vector3(-this.center.X, -this.center.Y, 0)) *
                 Matrix.CreateScale(new Vector3(this.zoom, this.zoom, 0)) *
-                Matrix.CreateTranslation(new Vector3(view.Width/2, view.Height/2, 0));
-                
+                Matrix.CreateTranslation(new Vector3(view.Width / 2, view.Height / 2, 0));
+
+        }
+
+        public void getZoomInput()
+        {
+
         }
     }
 }
 
+s
